e00f45b4b0c095c56bd8195afb5783b1ae1260cb
Marcos Vinicius de Oliveira <102962949+ViniMarkos283@users.noreply.github.com>
2022-10-28 10:53:40 -0300

    adaptaçoes

--- form.js
+++ form.js
@@ -6,6 +6,7 @@ const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/; // Este é 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     nameValidate();
+    RAValidate();
     emailValidate();
     mainPasswordValidate();
     comparePassword();
@@ -32,8 +33,8 @@ function nameValidate(){ // função que valida o nome
     }
 }
 
-function emailValidate(){ // função que valida o email
-    if(!emailRegex.test(campos[1].value))
+function RAValidate(){ // função que valida o RA
+    if(campos[1].value.length != 13)
     {
         setError(1);
     }
@@ -43,20 +44,31 @@ function emailValidate(){ // função que valida o email
     }
 }
 
-function mainPasswordValidate(){ //função que valida a senha
-    if(campos[2].value.length < 8)
+function emailValidate(){ // função que valida o email
+    if(!emailRegex.test(campos[2].value))
     {
         setError(2);
     }
-    else{
+    else
+    {
         removeError(2);
     }
 }
 
-function comparePassword(){ //compara os dois campos de senha
-    if(campos[2].value == campos[3].value && campos[3].value.length >= 8) {
-        removeError(3);
-    }else {
+function mainPasswordValidate(){ //função que valida a senha
+    if(campos[3].value.length < 8)
+    {
         setError(3);
+    }
+    else{
+        removeError(3);
+    }
+}
+
+function comparePassword(){ //compara os dois campos de senha
+    if(campos[3].value == campos[4].value && campos[3].value.length >= 8) {
+        removeError(4);
+    }else {
+        setError(4);
     }
 }
